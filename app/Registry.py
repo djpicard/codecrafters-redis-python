fbb68033458638f3625ed418ea7cad65310b55a0
DJ Picard <picard.daniel.cet@gmail.com>
2025-07-20 09:25:23 -0400

    adding type

---
+++ app/Registry.py
@@ -0,0 +1,46 @@
+"""command registry"""
+# command_registry.py
+import traceback
+from typing import Callable, Dict
+
+from .utils.encoder import encode
+
+
+class CommandRegistry:
+    """registry for all commands"""
+    def __init__(self):
+        """initialize an empty registry"""
+        self._commands: Dict[str, Callable[..., str]] = {}
+
+    def register(self, name):
+        """register a new command"""
+        def decorator(func):
+            """set the decorator for the function"""
+            self._commands[name.upper()] = func
+            return func
+        return decorator
+
+    def handle(self, command_line:str) -> str:
+        """handle function call"""
+        if not command_line:
+            return "Empty command"
+
+        parts = [x for x in command_line.split("\r\n")[1:-1] if not x.startswith("$")]
+        cmd = parts[0].upper()
+        args = parts[1:]
+
+        print(f"CMD: {cmd}, args: {args}, parts: {parts}")
+        handler = self._commands.get(cmd)
+        if not handler:
+            print(f"Unknown command: {cmd}")
+            return "$-1"
+
+        try:
+            return encode(handler(*args))
+        except Exception as e: # pylint: disable=broad-exception-caught
+            print(f"Error handling command {cmd}: {str(e)}")
+            print(traceback.format_exc())
+            return "$-1"
+
+# Singleton instance
+registry = CommandRegistry()
